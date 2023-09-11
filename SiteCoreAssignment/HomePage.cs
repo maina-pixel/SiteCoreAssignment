188605461bd73796de4064ea6e90f3c9ee103b15
Maina Krishna <Maina Krishna@LAPTOP-OE9U9QGT>
2023-09-11 18:59:08 +0800

    Add project files.

---
+++ SiteCoreAssignment/HomePage.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SiteCoreAssignment
+{
+    public class HomePage
+    {
+        private IWebDriver driver;
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        // Elements on the Home page
+        public void GoTo(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+
+
+        }
+
+        public void EnterTextInSearchBox(string searchText)
+        {
+            //System.Threading.Thread.Sleep(2000);
+            driver.FindElement(By.Id("twotabsearchtextbox")).SendKeys(searchText);
+        }
+
+        public void ClickSearchButton()
+        {
+            System.Threading.Thread.Sleep(3000);
+            driver.FindElement(By.Id("nav-search-submit-button")).Click();
+
+        }
+    }
+
+}
+    
+
+
+
+
+
+   
---
+++ SiteCoreAssignment/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Globalization;
+
+namespace SiteCoreAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+            try
+            {
+                // Step 1: User arrives at Amazon's main page
+                // *******************************************
+                HomePage homePage = new HomePage(driver);
+                homePage.GoTo("https://www.amazon.com/");
+
+                System.Threading.Thread.Sleep(10000);
+
+                // Step 2: Perform a search
+                // ******************************************
+                homePage.EnterTextInSearchBox("Laptop");
+                homePage.ClickSearchButton();
+
+                // Wait for search results to load
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+               
+
+                // Select the first result (index 0)
+                int selectedIndex = 0;
+                IList<IWebElement> resultLinks = driver.FindElements(By.CssSelector(".s-main-slot .s-result-item a"));
+                if (resultLinks.Count > selectedIndex)
+                {
+                   Console.WriteLine($"Selected index: {selectedIndex}");
+                   resultLinks[selectedIndex].Click();
+                   string priceText = driver.FindElement(By.CssSelector(".a-price .a-offscreen")).Text;
+                    decimal price;
+
+                    if (decimal.TryParse(priceText.Trim('$', ' '), NumberStyles.Currency, CultureInfo.InvariantCulture, out price))
+                    {
+                        // Assert that the price is more than 100
+                        if (price > 100)
+                        {
+                            Console.WriteLine($"Price: ${price} is more than 100.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Price: ${price} is not more than 100.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unable to parse price.");
+                    }
+ 
+                }
+                else
+                {
+                    Console.WriteLine("No result links found or the selected index is out of range.");
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            finally
+            {
+                
+                driver.Quit();
+            }
+        }
+    }
+}
